c7f0229fff352255d6c46734164d78b878fb8739
courage331 <courage331@gmail.com>
2019-12-10 11:19:24 +0900

    ServerUI_GamebuttonPosition
    ServerUI_GamebuttonPosition

--- Client/ShopScene.gd
+++ Client/ShopScene.gd
@@ -1,4 +1,5 @@
 extends Control
+var count = 0
 
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +9,14 @@ func _on_Button_pressed():
 
 func _on_Button2_pressed():
 	Global.goto_scene("res://SceneFolder/MainScene.tscn")
+	
+func _on_Button3_pressed():
+	if(count %2==0):
+		get_node("Explain_sprite").visible = true
+		count +=1
+	else:
+		get_node("Explain_sprite").visible = false
+		count +=1
+
+func _on_Button4_pressed():
+	pass
